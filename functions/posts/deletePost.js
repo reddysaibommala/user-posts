f122c395b36f820a1cecaeb63c45fa26f12a1e1c
reddysai <reddysai@agiratech.com>
2021-03-07 16:16:48 +0530

    updated authentication format into middleware

--- functions/posts/deletePost.js
+++ functions/posts/deletePost.js
@@ -2,13 +2,16 @@ const express = require("express");
 const router = express.Router();
 const upload = require('./../../lib/upload');
 const { Post } = require('./../../models/post');
+const { Like } = require('./../../models/like');
 const { validateRecord } = require('./middlewares');
+const { checkIfAuthenticated } = require('./../../lib/authenticate');
 
 router.delete(
   '/:id',
+  checkIfAuthenticated,
   validateRecord,
   function(req, res, next) {
-    let { record } = res.locals|| {};
+    let { record } = res.locals || {};
     if ( record &&
       record.attachment &&
       record.attachment.key) {
@@ -19,10 +22,11 @@ router.delete(
     } else next();
   },
   function(req, res, next) {
-    let { query } = res.locals|| {};
+    let { query, record } = res.locals|| {};
     Post.deleteOne(query)
-    .then((response) => {
-      res.status(200).send({message: 'Note deleted successfully'});
+    .then(async (response) => {
+      await Like.deleteMany({ postId: record._id});
+      res.status(200).send({message: 'Post deleted successfully'});
     })
     .catch(err => {
       res.status(400).send({ error: err.message });
--- functions/posts/listPosts.js
+++ functions/posts/listPosts.js
@@ -4,13 +4,46 @@ const router = express.Router();
 const { Post } = require('./../../models/post');
 const { validateRecord } = require('./middlewares');
 
+const formatPagination = ({ query = {}}, res, next) => {
+  let {
+    perPage = 10, page = 1
+  } = query;
+
+  if (+page <= 0) {
+    page = 1;
+  }
+
+  let skip = (page - 1) * perPage;
+
+  res.locals.pagination = {
+    limit: +perPage,
+    skip: +skip,
+    page: +page
+  };
+
+  next();
+}
+
 router.get(
   '/',
-  function(req, res, next) {
-    let { query = {}} = res.locals|| {};
+  formatPagination,
+  async function(req, res, next) {
+    let { query = {}, pagination} = res.locals|| {};
+    let sort = {
+      createdAt: 1,
+      points: 1
+    }
+    let total = await Post.countDocuments(query);
     Post.find(query)
+    .collation({locale: "en"})
+    .sort(sort)
+    .skip(pagination.skip)
+    .limit(pagination.limit)
     .then((result) => {
-      res.status(200).send(result);
+      res.status(200).send({
+        posts: result,
+        pagination: Object.assign(pagination, { total: total })
+      });
     })
     .catch(next)
   }
